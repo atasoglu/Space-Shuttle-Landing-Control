31eda2c2cc80492d2df895f21f42c7b978df07ce
Ahmet Ataşoğlu <ahmetatasoglu98@gmail.com>
2018-12-27 00:00:28 +0300

    Add files via upload

--- knn.py
+++ knn.py
@@ -12,20 +12,73 @@ class knn:
     
     def __init__(self, k_value, target_value, feature_num):
         self.k = k_value # k parameter - how many neighbors to algorithm will consider
+        # will be added soon!
         self.data = target_value
         self.f_num = feature_num
+        self.default_min = 10
+        self.classes = [2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2]
+        self.value_of_nn = 0
         # print(self.k)
 
-    def get_col_elements(self, col_num):
-        val = []
+    def get_distance(self, dist, examp_val):
+        
+        x = 0
+        for y,z in zip(dist, examp_val):
+            x += pow(int(y)-int(z), 2)
+        return sqrt(x)
+
+    def fit_nn(self, sample_values):
+        
+        arr = sample_values
+        temp_sum = 0 # temporary sum of on a row
+        result_arr = []
         for row in self.data:
-            val.append(row[col_num])
-        return val
+            row.pop(0)
+            temp_sum = self.get_distance(row, arr)
+            result_arr.append(temp_sum)
+        """
+        # to show all values
+        for t in result_arr:
+            print(t)
+        """
 
-    def get_distance(self, x, y):
-        return sqrt(pow(x-y, 2))
+        return result_arr
+        
+    def get_nn(self, results):
+        
+        arr = results
+        # print(arr, len(arr))
+        min_val = self.default_min
+        i=0
+        for x in arr:
+            if x < min_val:
+                min_val = x
+                count = i
+            i+=1
+        self.value_of_nn = arr[count]
+        return count
+        
+    
+        
+        """
+        arr = sample_values # incoming example
+        sum_distances 
+        min_distance = self.default_min
+        for i in range(1, self.f_num):
+            for ele in arr: # each example
+                distances = []
+                j=0 # row number of specified column's data
+                
+                for dt in self.get_col_elements(i): # each data on specified dataset
+                    temp_distance = self.get_distance(int(ele), int(dt))
+                    distances.append(temp_distance)
+                    if temp_distance < min_distance:
+                        count = j
+        """            
 
-    def find_nn(self, sample_values):
+                
+
+        """
         arr = sample_values
         dt = []
         nn_count = 0
@@ -46,6 +99,7 @@ class knn:
                 j+=1
                 # print(distance)
             # print("\n")
+        """
         
             
 
--- main.py
+++ main.py
@@ -12,9 +12,11 @@
         daha fazla detay için, /docs dizinideki dökümanı okuyabilirsiniz.
         
         author: ahmet atasoglu @ 2018
+
+        # for testing the algorithm, please follow the instroductions #
 """
 
-# print(__doc__)
+print(__doc__)
 
 import csv
 from imputing import *
@@ -35,9 +37,33 @@ imp = imputing(data)
 data = imp.handle_missing() # missing values will be change by this operation
 # the method that we use to change values is the mod. 
 # for more, look ~ imputing.py
-k = knn(2, data, 7)
-s = [1,2,3,4,6,7]
-k.find_nn(s)
+kNN = knn(1, data, 7)
+train = []
+
+# creating training set by your answers...
+a = input("1) STABILITY: stab or xstab\n'1' to stab, '2' to xstab: ")
+train.append(int(a))
+a = input("2) ERROR: XL, LX, MM or SS\n'1' to XL, '2' to LX, '3' to MM, '4' to SS: ")
+train.append(int(a))
+a = input("3) SIGN: pp or nn\n'1' to pp, '2' to nn: ")
+train.append(int(a))
+a = input("4) WIND: head or tail\n'1' to head, '2' to tail: ")
+train.append(int(a))
+a = input("5) MAGNITUDE: Low, Medium, Strong or OutOfRange\n'1' to Low, '2' to Medium, '3' to Strong, '4' to OutOfRange: ")
+train.append(int(a))
+a = input("6) VISIBILITY: yes or no\n'1' to yes, '2' to no: ")
+train.append(int(a))
+
+        
+# ...and machine answers the human's question :)
+
+print("\n### Machine Says: ###")
+result = kNN.get_nn(kNN.fit_nn(train))
+if kNN.classes[result] == 1:
+        print("shuttle must be manuel controlled!")
+elif kNN.classes[result] == 2:
+        print("shuttle must be auto controlled!")
+
 """
 # use to show newly dataset with repaired datas
 for i in data:
